82f4cb275a50c393672656279419de769b823229
ljacqu <ljacqu@users.noreply.github.com>
2015-08-31 22:51:22 +0200

    Created more thorough test for dictionary sanitation

--- test/ch/ljacqu/wordeval/language/DictionarySanitationTest.java
+++ test/ch/ljacqu/wordeval/language/DictionarySanitationTest.java
@@ -1,12 +1,16 @@
 package ch.ljacqu.wordeval.language;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import java.io.IOException;
-import java.time.temporal.UnsupportedTemporalTypeException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import org.junit.Ignore;
 import org.junit.Test;
+import ch.ljacqu.wordeval.LetterService;
+import ch.ljacqu.wordeval.LetterType;
 import ch.ljacqu.wordeval.evaluation.Evaluator;
 import ch.ljacqu.wordeval.evaluation.PartWordEvaluator;
 
@@ -15,8 +19,8 @@ public class DictionarySanitationTest {
   private static final String languageCode = "hu";
 
   @Test
-  public void shouldNotReturnNonWordChars() throws IOException {
-    TestEvaluator testEvaluator = new TestEvaluator();
+  public void shouldNotHaveForbiddenChars() throws IOException {
+    ForbiddenCharsEvaluator testEvaluator = new ForbiddenCharsEvaluator();
     List<Evaluator> evaluators = Collections.singletonList(testEvaluator);
     Dictionary dictionary = Dictionary.getLanguageDictionary(languageCode,
         evaluators);
@@ -27,15 +31,41 @@ public class DictionarySanitationTest {
     if (!errors.isEmpty()) {
       fail("Found " + errors.size() + " errors: " + errors);
     }
+    assertTrue(errors.isEmpty());
   }
 
-  private static class TestEvaluator extends PartWordEvaluator {
+  @Test
+  @Ignore
+  // TODO #20: Make test work with according sanitation
+  public void shouldNotHaveNonWordCharacters() throws IOException {
+    List<Character> allowedChars = new ArrayList<>();
+    allowedChars.add('-');
+    allowedChars.addAll(LetterService.getLetters(LetterType.VOWELS));
+    allowedChars.addAll(LetterService.getLetters(LetterType.CONSONANTS));
+    NoOtherCharsEvaluator testEvaluator = new NoOtherCharsEvaluator(
+        allowedChars);
 
+    List<Evaluator> evaluators = Collections.singletonList(testEvaluator);
+    Dictionary dictionary = Dictionary.getLanguageDictionary(languageCode,
+        evaluators);
+
+    dictionary.processDictionary();
+    Map<String, List<String>> errors = testEvaluator.getResults();
+
+    if (!errors.isEmpty()) {
+      testEvaluator.outputAggregatedResult();
+      fail("Found errors");
+    } else {
+      assertTrue(errors.isEmpty());
+    }
+  }
+
+  private static class ForbiddenCharsEvaluator extends PartWordEvaluator {
     private static final char[] ILLEGAL_CHARACTERS = { '/', ',', '\n', '(',
         ')', '[', ']', ' ', '!', '?', '_', '0', '1', '2', '3', '4', '5', '6',
         '7', '8', '9' };
 
-    private List<String> errors = new ArrayList<String>();
+    private List<String> errors = new ArrayList<>();
 
     @Override
     public void processWord(String word, String rawWord) {
@@ -49,6 +79,28 @@ public class DictionarySanitationTest {
     public List<String> getErrors() {
       return errors;
     }
+  }
 
+  private static class NoOtherCharsEvaluator extends PartWordEvaluator {
+    private final List<Character> allowedChars;
+
+    public NoOtherCharsEvaluator(List<Character> allowedChars) {
+      this.allowedChars = allowedChars;
+    }
+
+    @Override
+    public void processWord(String word, String rawWord) {
+      for (int i = 0; i < word.length(); ++i) {
+        char curChar = word.charAt(i);
+        if (!allowedChars.contains(curChar)) {
+          addEntry(new Character(curChar).toString(), word);
+        }
+      }
+    }
+
+    @Override
+    public WordForm getWordForm() {
+      return WordForm.NO_ACCENTS;
+    }
   }
 }
