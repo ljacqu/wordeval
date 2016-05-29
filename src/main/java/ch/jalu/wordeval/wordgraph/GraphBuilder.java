6944afc836979562698887d6e8f2585482b15df7
ljacqu <ljacqu@users.noreply.github.com>
2016-05-29 17:01:37 +0200

    Use faster Damerau-Levenshtein algorithm implementation

--- src/main/java/ch/jalu/wordeval/wordgraph/GraphBuilder.java
+++ src/main/java/ch/jalu/wordeval/wordgraph/GraphBuilder.java
@@ -1,13 +1,12 @@
 package ch.jalu.wordeval.wordgraph;
 
-import java.util.List;
-
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.SimpleGraph;
-
 import ch.jalu.wordeval.evaluation.WordCollector;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import java.util.List;
 
 /**
  * Finds which words have a Damerau-Levenshtein distance of 1 and saves these
@@ -15,9 +14,6 @@ import lombok.extern.log4j.Log4j2;
  */
 @Log4j2
 public class GraphBuilder {
-
-  /** The minimum distance for words to be connected. */
-  private static final int MIN_DISTANCE = 1;
   
   /** 
    * The interval in which to display the words that have been processed.
@@ -53,15 +49,12 @@ public class GraphBuilder {
 
   private void constructGraph(List<String> words) {
     graph = new SimpleGraph<>(DefaultWeightedEdge.class);
-    // deleteCost, insertCost, replaceCost, swapCost    
-    DamerauLevenshteinAlgorithm levenshtein = new DamerauLevenshteinAlgorithm(1, 1, 1, 1);
     for (int i = 0; i < words.size(); ++i) {
       final String leftWord = words.get(i);
       graph.addVertex(leftWord);
       for (int j = i + 1; j < words.size(); ++j) {
         final String rightWord = words.get(j);
-        if (Math.abs(rightWord.length() - leftWord.length()) <= MIN_DISTANCE
-            && levenshtein.execute(rightWord, leftWord) <= MIN_DISTANCE) {
+        if (DamerauLevenshtein.isEditDistance1(rightWord, leftWord)) {
           // vertices must always be added before edges. addVertex() checks against a Set,
           // so the check is efficient enough for us to just always call the functions
           graph.addVertex(rightWord);
