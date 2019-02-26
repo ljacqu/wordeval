9120b428d0c423c77df7fa0775170325254ef3b2
ljacqu <ljacqu@users.noreply.github.com>
2019-02-26 22:58:30 +0100

    Improve instantiation of evaluators

--- src/main/java/ch/jalu/wordeval/runners/EvaluatorInitializer.java
+++ src/main/java/ch/jalu/wordeval/runners/EvaluatorInitializer.java
@@ -4,8 +4,9 @@ import ch.jalu.wordeval.evaluation.Evaluator;
 import ch.jalu.wordeval.language.Language;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
 
-import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
@@ -13,6 +14,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Service for initializing all available evaluators.
@@ -20,8 +23,7 @@ import java.util.Locale;
 @Log4j2
 public class EvaluatorInitializer {
 
-  private static final String EVALUATOR_FOLDER = "src/main/java/ch/jalu/wordeval/evaluation";
-  private static final String EVALUATOR_PACKAGE = "ch.jalu.wordeval.evaluation.";
+  private static final String EVALUATOR_PACKAGE = "ch.jalu.wordeval.evaluation";
 
   private final Language language;
   @Getter
@@ -38,42 +40,26 @@ public class EvaluatorInitializer {
   }
 
   /**
-   * Builds a list of all available evaluators located in the {@link #EVALUATOR_FOLDER evaluators folder}.
+   * Creates all possible evaluators from the evaluator package, {@link #EVALUATOR_PACKAGE}.
    */
   private void buildAllEvaluators() {
-    File[] files = new File(EVALUATOR_FOLDER).listFiles();
-    if (files == null || files.length == 0) {
-      throw new IllegalStateException("Could not read folder '" + EVALUATOR_FOLDER + "'");
-    }
-    for (File file : files) {
-      if (file.isFile() && file.getName().endsWith(".java")) {
-        instantiateEvaluator(getClassFromFile(file));
-      }
-    }
+    Reflections reflections = new Reflections(EVALUATOR_PACKAGE, new SubTypesScanner());
+
+    Set<Class<? extends Evaluator>> evaluatorClasses = reflections.getSubTypesOf(Evaluator.class).stream()
+        .filter(clz -> !clz.isInterface() && !Modifier.isAbstract(clz.getModifiers()))
+        .filter(clz -> !clz.isMemberClass() || !clz.getDeclaringClass().getName().endsWith("Test"))
+        .collect(Collectors.toSet());
+
+    evaluatorClasses.forEach(this::instantiateEvaluator);
   }
 
-  private void instantiateEvaluator(Class<? extends Evaluator<?>> clazz) {
+  private void instantiateEvaluator(Class<? extends Evaluator> clazz) {
     if (clazz != null) {
       Constructor<? extends Evaluator<?>> constructor =
           (Constructor<? extends Evaluator<?>>) clazz.getDeclaredConstructors()[0];
       log.debug("Resolving instantiation of class '{}'", clazz.getSimpleName());
       createEvaluators(constructor, new ArrayList<>(Arrays.asList(constructor.getParameterTypes())), new ArrayList<>());
     }
-  }
-
-  private Class<? extends Evaluator<?>> getClassFromFile(File file) {
-    String className = EVALUATOR_PACKAGE + file.getName();
-    try {
-      // strip ".java"
-      Class<?> clazz = Class.forName(className.substring(0, className.length() - 5));
-      if (!clazz.isInterface() && !Modifier.isAbstract(clazz.getModifiers())
-          && Evaluator.class.isAssignableFrom(clazz)) {
-        return (Class<? extends Evaluator<?>>) clazz;
-      }
-    } catch (ClassNotFoundException e) {
-      throw new IllegalStateException("Could not load class '" + className + "'");
-    }
-    return null;
   }
 
   private void createEvaluators(Constructor<? extends Evaluator<?>> constructor,
@@ -83,19 +69,15 @@ public class EvaluatorInitializer {
       evaluators.add(newInstance(constructor, resolvedDependencies));
       return;
     }
+
     Class<?> dependency = dependencies.remove(0);
     Object[] resolvedDependency = resolveDependency(dependency);
-    if (resolvedDependency.length == 1) {
-      resolvedDependencies.add(resolvedDependency[0]);
-      createEvaluators(constructor, dependencies, resolvedDependencies);
-    } else {
-      for (Object dep : resolvedDependency) {
-        // Copy lists so each call has its own copy
-        List<Class<?>> dependenciesCopy = new ArrayList<>(dependencies);
-        List<Object> resolvedDependenciesCopy = new ArrayList<>(resolvedDependencies);
-        resolvedDependenciesCopy.add(dep);
-        createEvaluators(constructor, dependenciesCopy, resolvedDependenciesCopy);
-      }
+    for (Object dep : resolvedDependency) {
+      // Copy lists so each call has its own copy
+      List<Class<?>> dependenciesCopy = new ArrayList<>(dependencies);
+      List<Object> resolvedDependenciesCopy = new ArrayList<>(resolvedDependencies);
+      resolvedDependenciesCopy.add(dep);
+      createEvaluators(constructor, dependenciesCopy, resolvedDependenciesCopy);
     }
   }
 
