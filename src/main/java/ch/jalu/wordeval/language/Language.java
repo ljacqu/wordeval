a8d04d0018a7be1bb985caba10c0865e31c7a98f
ljacqu <ljacqu@users.noreply.github.com>
2025-04-18 18:17:29 +0200

    Rename standardVowels to defaultVowels, remove unused method

--- src/main/java/ch/jalu/wordeval/language/Language.java
+++ src/main/java/ch/jalu/wordeval/language/Language.java
@@ -2,11 +2,11 @@ package ch.jalu.wordeval.language;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static java.util.Arrays.asList;
 import static java.util.Objects.requireNonNull;
 
@@ -68,14 +68,14 @@ public class Language {
   }
 
   public List<String> getVowels() {
-    List<String> vowels = newArrayList(alphabet.getStandardVowels());
+    List<String> vowels = new ArrayList<>(alphabet.getDefaultVowels());
     vowels.removeAll(lettersToRemove);
     vowels.addAll(additionalVowels);
     return vowels;
   }
 
   public List<String> getConsonants() {
-    List<String> consonants = newArrayList(alphabet.getStandardConsonants());
+    List<String> consonants = new ArrayList<>(alphabet.getDefaultConsonants());
     consonants.removeAll(lettersToRemove);
     consonants.addAll(additionalConsonants);
     return consonants;
