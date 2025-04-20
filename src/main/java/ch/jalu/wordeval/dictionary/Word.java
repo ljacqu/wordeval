e5b7684c905f952312187e364a403a2cf19691b0
ljacqu <ljacqu@users.noreply.github.com>
2025-04-20 17:14:02 +0200

    Document all word forms with examples

--- src/main/java/ch/jalu/wordeval/dictionary/Word.java
+++ src/main/java/ch/jalu/wordeval/dictionary/Word.java
@@ -15,9 +15,32 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class Word {
 
+  /**
+   * Word in its original form, as read from the dictionary.
+   * <p>
+   * Examples: cartagüeño (es), aujourd'hui (fr), Loftshøjgård (da)
+   */
   private String raw;
+
+  /**
+   * Word in its locale-appropriate all lower case form (e.g. Turkish {@code I} becomes {@code ı}).
+   * <p>
+   * Examples: cartagüeño (es), aujourd'hui (fr), loftshøjgård (da)
+   */
   private String lowercase;
+
+  /**
+   * Word all in lower case without any diacritics that aren't considered separate letters by the language in question.
+   * <p>
+   * Examples: cartagueño (es), aujourd'hui (fr), loftshøjgård (da)
+   */
   private String withoutAccents;
+
+  /**
+   * Word all in lower case without any diacritics or any non-letters (like hyphens, apostrophes).
+   * <p>
+   * Examples: cartagueño (es), aujourdhui (fr), loftshøjgård (da)
+   */
   private String withoutAccentsWordCharsOnly;
 
 }
